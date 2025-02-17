4b5cb5fa9b07ccd5c99e2ca72b0c812bf673febf
gab <114111654+gabalbcos@users.noreply.github.com>
2025-02-17 18:10:31 -0300

    add buttons and useState functionality

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -2,24 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return(
-    <>
-    <p>Movie Title: {title}</p>
-    </>
+    <div className='card'>
+    <h2>Movie Title: {title}</h2>
+    <button onClick={() => (setHasLiked(true))}>
+     {hasLiked ? 'Liked' : 'Like'} 
+      </button>
+    </div>
   )
 }
 
 function App() {
 
   return (
-    <>
-     <h2>Title</h2>
+    <div className='card-container'>
      <Card title="Star Wars"/>
      <Card title="Avatar"/>
      <Card title="Lion King"/>
-
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
