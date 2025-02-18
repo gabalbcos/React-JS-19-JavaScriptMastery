1845e496382cbed6ab687941b8def0bf49b16bc2
gab <114111654+gabalbcos@users.noreply.github.com>
2025-02-18 11:36:29 -0300

    add new project files

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const Card = ({title}) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
 
+useEffect (() => {
+  console.log(`${title} has been liked: ${hasLiked}`);
+}, [hasLiked]); 
+
   return(
-    <div className='card'>
-    <h2>Movie Title: {title}</h2>
-    <button onClick={() => (setHasLiked(true))}>
+    <div className='card' onClick={() => setCount(count + 1)}>
+    <h2>Movie Title: {title}<br/> {count || null}</h2>
+    
+    <button onClick={() => (setHasLiked(!hasLiked))}>
      {hasLiked ? 'Liked' : 'Like'} 
       </button>
     </div>
