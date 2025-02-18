a0cff446daa90c53bce3613885677c9f715615c2
gab <114111654+gabalbcos@users.noreply.github.com>
2025-02-18 18:02:15 -0300

    add initial style + typescript to project

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,34 +1,18 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import React from 'react'
 
-const Card = ({title}) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-useEffect (() => {
-  console.log(`${title} has been liked: ${hasLiked}`);
-}, [hasLiked]); 
-
-  return(
-    <div className='card' onClick={() => setCount(count + 1)}>
-    <h2>Movie Title: {title}<br/> {count || null}</h2>
-    
-    <button onClick={() => (setHasLiked(!hasLiked))}>
-     {hasLiked ? 'Liked' : 'Like'} 
-      </button>
-    </div>
-  )
-}
-
-function App() {
-
+const App = () => {
   return (
-    <div className='card-container'>
-     <Card title="Star Wars"/>
-     <Card title="Avatar"/>
-     <Card title="Lion King"/>
+    <main>
+      
+      <div className="pattern" />
+    <div className="wrapper">
+      <header>
+        <h1>Find Movies You'll Enjoy Without the Hassle</h1>
+      </header>
     </div>
-  );
+
+    </main>
+  )
 }
 
 export default App
