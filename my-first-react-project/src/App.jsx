5b6c19a7e46610693cc1637be9505b443e74de56
gab <114111654+gabalbcos@users.noreply.github.com>
2025-02-20 15:26:43 -0300

    add debounce to Search Term

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { Search } from './components/Search'
 import Spinner from './components/Spinner'
 import { MovieCard } from './components/MovieCard';
+import { useDebounce } from 'react-use'
+
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -21,12 +23,17 @@ const[searchTerm, setSearchTerm] = useState('');
 const [errorMessage, setErrorMessage] = useState('');
 const [movieList, setMovieList] = useState([]);
 const [isLoading, setIsLoading] = useState(false);
+const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-const fetchMovies = async () => {
+useDebounce( () => setDebouncedSearchTerm(searchTerm), 500, [searchTerm] );
+
+const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
   try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const endpoint = query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURI(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
 
       const response = await fetch(endpoint, API_OPTIONS);
       
@@ -52,8 +59,8 @@ const fetchMovies = async () => {
 }
 
 useEffect(() => {
-  fetchMovies();
-}, []);
+  fetchMovies(debouncedSearchTerm);
+}, [debouncedSearchTerm]);
 
   return (
     <main>
